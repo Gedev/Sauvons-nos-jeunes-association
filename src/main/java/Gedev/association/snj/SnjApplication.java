d3021bba170a63f737fe90d0890794e63ebf5548
Gérald Devreux <gerald.devreux.pro@gmail.com>
2022-01-19 15:58:29 +0100

    Project init

---
+++ src/main/java/Gedev/association/snj/SnjApplication.java
@@ -0,0 +1,13 @@
+package Gedev.association.snj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Gedev/association/snj/SnjApplicationTests.java
@@ -0,0 +1,13 @@
+package Gedev.association.snj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
