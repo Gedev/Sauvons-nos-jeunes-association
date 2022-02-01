e70913aa1c4978fcf621ddf0bcc75745f249b59b
SamiMas24 <samirahmasimina@gmail.com>
2022-02-01 13:12:44 +0100

    modife config docker

--- src/main/java/Gedev/association/snj/SnjApplication.java
+++ src/main/java/Gedev/association/snj/SnjApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SnjApplication {
-
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(SnjApplication.class, args);
 	}
 
